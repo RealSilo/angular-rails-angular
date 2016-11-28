0f89b884124c3deb9a51ad5cbe7b71a9095186d9
RealSilo <szilard.magyar@gmail.com>
2016-11-29 00:09:05 +0100

    new proposal

--- app/app.module.ts
+++ app/app.module.ts
@@ -11,6 +11,7 @@ import { DocumentService } from './documents/document.service';
 import { ProposalListComponent } from  './proposal/proposal-list.component';
 import { ProposalNewComponent } from  './proposal/proposal-new.component';
 import { ProposalShowComponent } from  './proposal/proposal-show.component';
+import { ProposalService } from './proposal/proposal.service';
 
 import { AppRoutingModule } from './app-routing.module';
 @NgModule({
@@ -18,6 +19,7 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpModule,
     NgbModule.forRoot()
   ],
   declarations: [
@@ -29,7 +31,8 @@ import { AppRoutingModule } from './app-routing.module';
     ProposalShowComponent
   ],
   providers: [
-    DocumentService
+    DocumentService,
+    ProposalService
   ],
   bootstrap: [
     AppComponent
--- app/proposal/proposal-list.component.js
+++ app/proposal/proposal-list.component.js
@@ -9,18 +9,28 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
-var proposal_1 = require("./proposal");
+var router_1 = require("@angular/router");
+var Rx_1 = require("rxjs/Rx");
+var proposal_service_1 = require("./proposal.service");
 var ProposalListComponent = (function () {
-    function ProposalListComponent() {
-        this.proposalOne = new proposal_1.Proposal(15, 'Abc Company', 'http://szmagyar.com', 'RoR', 123, 123, 14, 'szmagyar@gmail.com');
-        this.proposalTwo = new proposal_1.Proposal(16, 'asdf Company', 'http://szmagyar.com', 'RoR', 123, 123, 14, 'szmagyar@gmail.com');
-        this.proposalThree = new proposal_1.Proposal(17, 'zxcv Company', 'http://szmagyar.com', 'RoR', 123, 123, 14, 'szmagyar@gmail.com');
-        this.proposals = [
-            this.proposalOne,
-            this.proposalTwo,
-            this.proposalThree
-        ];
+    function ProposalListComponent(proposalService, router) {
+        this.proposalService = proposalService;
+        this.router = router;
     }
+    ProposalListComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        var timer = Rx_1.Observable.timer(0, 5000);
+        timer.subscribe(function () { return _this.getProposals(); });
+    };
+    ProposalListComponent.prototype.getProposals = function () {
+        var _this = this;
+        this.proposalService.getProposals()
+            .subscribe(function (proposals) { return _this.proposals = proposals; }, function (error) { return _this.errorMessage = error; });
+    };
+    ProposalListComponent.prototype.goToShow = function (proposal) {
+        var link = ['/proposals', proposal.id];
+        this.router.navigate(link);
+    };
     return ProposalListComponent;
 }());
 ProposalListComponent = __decorate([
@@ -28,9 +38,11 @@ ProposalListComponent = __decorate([
         moduleId: module.id,
         selector: 'proposal-list',
         templateUrl: 'proposal-list.component.html',
-        styleUrls: ['proposal-list.component.css']
+        styleUrls: ['proposal-list.component.css'],
+        providers: [proposal_service_1.ProposalService]
     }),
-    __metadata("design:paramtypes", [])
+    __metadata("design:paramtypes", [proposal_service_1.ProposalService,
+        router_1.Router])
 ], ProposalListComponent);
 exports.ProposalListComponent = ProposalListComponent;
 //# sourceMappingURL=proposal-list.component.js.map
